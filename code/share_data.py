c11f4d38db4943d5a411c178c1c9438cc73c0a96
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2022-09-28 13:13:16 -0400

    Create share_data.py
    for sharing select data publicly

---
+++ code/share_data.py
@@ -0,0 +1,38 @@
+# Initialize
+from pathlib import Path
+from shutil import copyfile
+
+p = Path(__file__)
+processed_dir = p.parents[1].joinpath("data", "processed")
+analysis_dir = p.parents[1].joinpath("data", "analysis")
+sharing_dir = p.parents[1].joinpath("data", "for_sharing")
+if sharing_dir.exists():
+    pass
+else:
+    try:
+        sharing_dir.mkdir(parents=True, exist_ok=True)
+    except:
+        print("Cannot create data directory.")
+
+# point to files to copy
+dir_list = [processed_dir, analysis_dir]
+file_list = ["public_inspection_midnight_documents_all.csv", 
+             "documents_by_agency_year.csv", 
+             "documents_by_year_type.csv", 
+             "documents_withdrawn_archived_checked.csv",
+             "nprm_data.csv", 
+             "rules_withdrawn_priority.csv"
+             ]
+
+# copy files to sharing_dir
+print(f"Attempting to copy {len(file_list)} files...")
+for d in dir_list:
+    for f in file_list:
+        check_file = d / f
+        if Path.is_file(check_file):
+            new_file = sharing_dir / f
+            copyfile(check_file, new_file)
+            print(f"Copied {f}")
+        else:
+            continue
+
